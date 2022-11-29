5613489bc148f9232b542566c63cb6e318c10321
vinodmangalore <vinodmangalore@gmail.com>
2022-11-29 20:18:21 +0530

    I am adding employee class

---
+++ EmployeeManagementWebApp/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeManagementWebApp
+{
+    public class Employee
+    {
+        public string Name { get;  set; }
+
+        public string Email { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
